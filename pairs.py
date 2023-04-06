6e6bc8d3f7e5b09fc7a847dce40b1dfdcc8b0bf8
Lily McMullen <122934612+lilymcmullen@users.noreply.github.com>
2023-04-06 12:19:24 -0700

    Adding print function to pairs.py

--- pairs.py
+++ pairs.py
@@ -49,3 +49,56 @@ for i in range(len(all_object_ids)-1):  # For each point except the last one
         # add the output raster name to the list
         output_rasters.append(output_raster_name)
 
+# import required packages
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+import csv
+import pandas as pd
+import os
+
+
+# Set the workspace environment to the folder containing the rasters
+arcpy.env.workspace = r"C:\Users\lilyb\OneDrive\Desktop\visibout2"
+
+# Use the ListRasters function to create a list of rasters in the folder
+rasters = arcpy.ListRasters()
+
+# Initialize variables to keep track of the maximum count value and the name of the raster with the maximum count value
+max_count = -1
+max_count_raster = ""
+
+# Iterate through the list of rasters
+for raster in rasters:
+    # Create a unique filename for the output dataframe
+    output_filename = f"{raster}_table.csv"
+
+    # Get the raster attribute table as a table view
+    table_view_name = f"{raster}_view"
+    table_view = arcpy.MakeTableView_management(raster, table_view_name)
+
+    # Get the Count value from each raster table using a SearchCursor
+    count_field = "Count"
+    with arcpy.da.SearchCursor(table_view, count_field) as cursor:
+        count_values = [row[0] for row in cursor]
+
+    # Find the maximum count value
+    count_sum = sum(count_values)
+
+    # Check if the Count value is greater than the current maximum count value
+    if count_sum > max_count:
+        # If so, update the maximum count value and the name of the raster with the maximum count value
+        max_count = count_sum
+        max_count_raster = raster
+
+    # Convert the table view to a Pandas dataframe
+    dataframe = pd.DataFrame.from_records(arcpy.da.TableToNumPyArray(table_view, count_field))
+
+    # Save the dataframe to a file
+    dataframe.to_csv(output_filename, index=False)
+
+    # Print a message indicating the dataframe has been saved
+    print(f"{output_filename} saved successfully.")
+
+# Print the name of the raster with the maximum count value
+print(f"Raster with the highest visibility is {max_count_raster} with a count of {max_count}")
