94b7668ae12152c3a35e325947017cce4f904542
amantaya <aantaya@email.arizona.edu>
2023-06-14 14:38:05 -0700

    rename script and Jupyter notebook to more descriptive filename

--- Scripts/Find-Optimal-Location-for-Single-Base-Station.py
+++ Scripts/Find-Optimal-Location-for-Single-Base-Station.py
@@ -7,7 +7,9 @@ import pandas as pd
 import os
 
 # set workspace - replace this with your project workspace/geodatabase (view HowToFindFilePaths.md on github if confused!)
-arcpy.env.workspace = r"C:\example\ArcGIS\Projects\folder\geodatabase.gdb"
+arcpy.env.workspace = r"C:\Users\Username\Documents\ArcGIS\Projects\folder\geodatabase.gdb"
+
+os.chdir(r"C:\Users\Username\Documents\ArcGIS\Projects\folder\geodatabase.gdb")
 
 # set location for raster files - replace this with any folder on your computer that can store your raster files.
 # If you aren't sure, put it on your desktop.
@@ -27,7 +29,7 @@ arcpy.env.overwriteOutput = True
 cropRaster = ExtractByMask(uncropRaster, bounds)
 cropRaster.save("cropRaster")
 
-# point selection- find local peaks within cropped raster.
+# find local peaks within cropped raster.
 # Default is 15 points, but set higher for more accuracy, and lower for faster processing time.
 pointsSelection = arcpy.defense.FindLocalPeaksValleys(cropRaster, "pointsSelection", "PEAKS", 15)
 
@@ -37,8 +39,11 @@ folder_name = "visibOutputs1"
 folder_path = os.path.join(folder, folder_name)
 
 # Create the new folder if it doesn't already exist
-if not os.path.exists(folder_path):
-    os.mkdir(folder_path)
+if not os.path.isdir(folder_path):
+    try:
+        os.mkdir(folder_path)
+    except OSError as error:
+        print(error)
     print(f"Folder '{folder_name}' created successfully!")
 
 # Set the current workspace to the new folder
@@ -47,20 +52,28 @@ arcpy.env.workspace = folder_path
 # set raster as cropRaster and points as pointsSelection for viewshed analysis
 feature_class = pointsSelection
 in_raster = cropRaster
+
 # so we can call ID field
 field = 'OBJECTID'
+
 # list all object IDs (from field = 'OBJECTID'
 all_object_ids = [row[0] for row in arcpy.da.SearchCursor(feature_class, field)]
+
 # Find the object ID fieldname
 objectidfield = arcpy.Describe(feature_class).OIDFieldName
+
 # looping through the points
 for pointid in all_object_ids:  # For each object id in the object id list
+
     # create a where clause to select only the point with this object ID
     sql = "{0}={1}".format(arcpy.AddFieldDelimiters(datasource=feature_class, field=objectidfield), pointid)
+
     # create a layer with only this point in it
     arcpy.MakeFeatureLayer_management(in_features=feature_class, out_layer='templayer', where_clause=sql)
+
     # run visibility analysis, with in_raster and templayer as observer. observer height is 6 (meters), but you can change the observer_offset value!
     outvis = arcpy.sa.Visibility(in_raster, 'templayer', analysis_type="OBSERVERS", nonvisible_cell_value="NODATA", observer_offset=6)
+
     # name the objects using object ID
     # I named them visibility_analysis_1.tif, visibility_analysis_2.tif etc...
     output_raster_name = os.path.join(folder_path,
